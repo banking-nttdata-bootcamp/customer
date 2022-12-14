50b1538eee4e8cb4ba87e28c6e773ec4271c0d1b
dmaldonf <dianacarolina8m@gmail.com>
2022-12-14 15:48:20 -0500

    update customer

--- src/main/java/com/nttdata/bootcamp/util/Constant.java
+++ src/main/java/com/nttdata/bootcamp/util/Constant.java
@@ -3,6 +3,7 @@ package com.nttdata.bootcamp.util;
 public class Constant {
     public static final String CUSTOMER_ACTIVE = "ACTIVE";
     public static final String CUSTOMER_INACTIVE = "INACTIVE";
+    public static final String CUSTOMER_DEBTOR = "DEBTOR";
     public static final String PERSONAL_CUSTOMER = "PERSONAL";
     public static final String BUSINESS_CUSTOMER = "EMPRESARIAL";
     public static final String QUERY_FAILED = "Error en la consulta";
