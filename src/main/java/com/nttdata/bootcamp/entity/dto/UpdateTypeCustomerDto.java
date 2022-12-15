bfcc5fb1ecbce137b0006c3baefa5001a5b95c5d
dmaldonf <dianacarolina8m@gmail.com>
2022-12-15 16:59:21 -0500

    update customer

---
+++ src/main/java/com/nttdata/bootcamp/entity/dto/UpdateTypeCustomerDto.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateTypeCustomerDto {
+    private Boolean flagVip;
+    private Boolean flagPyme;
+}
